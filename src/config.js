fade14cc5778bef5eb8ebc92043588fc0f1aa203
Wallace Coelho <wac.almeida@gmail.com>
2019-02-18 22:03:51 -0300

    Configure React APP to work with Amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-p4msbpsskxkp"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://t5degvrk54.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_jqNiV0fGH",
+    APP_CLIENT_ID: "6akgqilqttk7e7ufcm1jr17h2v",
+    IDENTITY_POOL_ID: "us-east-2:8e38b278-00b6-45e4-9ae5-51219ab44ba4"
+  }
+};
