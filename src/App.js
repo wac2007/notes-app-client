6d6c6e300cc9cb6bb3ea900bb3b4d2fa50418a9b
Wallace Coelho <wac.almeida@gmail.com>
2019-03-16 10:43:21 -0300

    Handle Logout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
 import Routes from "./Routes";
 import "./App.css";
@@ -11,14 +12,32 @@ class App extends Component {
     super(props);
     this.state = {
       isAuthenticated: false,
+      isAuthenticating: true
     }
   }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    } catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    this.setState({
+      isAuthenticating: false,
+    });
+  }
+  
   
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async () => {
+    await Auth.signOut();
     this.userHasAuthenticated(false);
   }
   
@@ -29,6 +48,7 @@ class App extends Component {
     };
 
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
