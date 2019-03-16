f5ecf08b9b54ab148983e05ffb0636d84b84664f
Wallace Coelho <wac.almeida@gmail.com>
2019-03-16 15:37:11 -0300

    Add route to note page

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from './containers/Signup';
 import NewNote from "./containers/NewNote";
-
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
   <Switch>
@@ -14,7 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     {/* <Route component={NotFound} /> */}
   </Switch>;
