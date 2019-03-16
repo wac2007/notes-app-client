690de935f35772b5cf9bf743b930343132f1fe3a
Wallace Coelho <wac.almeida@gmail.com>
2019-03-16 14:18:30 -0300

    Connect Signup with Cognito

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -5,6 +5,8 @@ import {
   FormControl,
   ControlLabel
 } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+
 import LoaderButton from "../components/LoaderButton";
 import "./Signup.css";
 
@@ -45,7 +47,17 @@ export default class Signup extends Component {
 
     this.setState({ isLoading: true });
 
-    this.setState({ newUser: "test" });
+    try {
+      const newUser = await Auth.signUp({
+        username: this.state.email,
+        password: this.state.password
+      });
+      this.setState({
+        newUser
+      })
+    } catch (e) {
+      alert(e.message);
+    }
 
     this.setState({ isLoading: false });
   }
@@ -54,6 +66,19 @@ export default class Signup extends Component {
     event.preventDefault();
 
     this.setState({ isLoading: true });
+
+    try {
+      await Auth.confirmSignUp(this.state.email, this.state.confirmationCode);
+      await Auth.signIn(this.state.email, this.state.password);
+
+      this.props.userHasAuthenticated(true);
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+    }
+
+    this.setState({ isLoading: false });
+
   }
 
   renderConfirmationForm() {
