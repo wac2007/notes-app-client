abcf961387456df324aa77ae5cb5806991187f8b
Wallace Coelho <wac.almeida@gmail.com>
2019-03-16 16:25:19 -0300

    Update lander on home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-
+import { Link } from "react-router-dom";
 import { API } from 'aws-amplify';
 
 import "./Home.css";
@@ -63,6 +63,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
