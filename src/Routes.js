93dc5f14af69a857e7d2b874f2e61c15b2871d4b
Wallace Coelho <wac.almeida@gmail.com>
2019-03-16 16:05:54 -0300

    Redirect proteccted pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch } from "react-router-dom";
 
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import AppliedRoute from './components/AppliedRoute';
 import Home from "./containers/Home";
 import Login from "./containers/Login";
@@ -11,10 +13,10 @@ import Notes from "./containers/Notes";
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
-    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     {/* <Route component={NotFound} /> */}
   </Switch>;
